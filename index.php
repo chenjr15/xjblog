55bee3f42d632877addf0f4cbbf9cf46273058c2
chenjinrong15@foxmail.com <chenjinrong15@foxmail.com>
2017-10-07 23:07:44 +0800

    style tweak

--- index.php
+++ index.php
@@ -5,11 +5,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>XJBlog</title>
+
 </head>
+
 <div style="width:60%;margin: auto ;">
     <div  style="background:#8d8d8d; height:100px"  >
         <div style="float:left;padding-left:45px;">
-            <h1 style="color:DeepSkyBlue;">XJBlog</h1>
+            <h1 style="color:white;" ><a href = "/" class="head"> XJBlog</a></h1>
         </div>
 
         <div style = "text-align:center;float:right;padding: 15px; ">
@@ -38,13 +40,13 @@
 
 </div>
 
- 
 
+<style type="text/css">
 
-<frame src="showpost.php">
-
-<?php
-
-?>
-
+        a.head:link, a.head:visited, a.head:hover, a.head:active {
+            color:  white;
+            text-decoration: none
+            }
+   
+</style>
 </html>
