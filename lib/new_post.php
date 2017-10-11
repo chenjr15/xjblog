922e811568e9065de7ff2bc2efde518651eb1c7d
icjr@qq.com <icjr@qq.com>
2017-10-12 00:13:13 +0800

    direct log to logfile

--- lib/new_post.php
+++ lib/new_post.php
@@ -20,7 +20,7 @@ date_default_timezone_set('PRC');
 $datetime= date("Y-m-d H:i:s");
 $dbh = connectDB();
 $sql = "INSERT INTO `post` (`id`, `title`, `content`, `time`, `uid`) VALUES (NULL, '".$_POST['title']."' ,'".$_POST['content']."', '".$datetime." ', '".$uid."' );";
-echo $sql;
+tolog($sql);
 $ret = $dbh->query($sql);
 $dbh=null;
 //var_dump($ret);
